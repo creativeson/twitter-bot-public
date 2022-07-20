b98fb8d0d5a3df3388f5f3f24bfc1e282aa790ba
creativeson <999demolish@gmail.com>
2022-07-20 12:00:53 +0800

    store data at mongodb

--- public_bot.py
+++ public_bot.py
@@ -1,35 +1,43 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb+srv://<secret>@cluster0.7wpz3kg.mongodb.net/?retryWrites=true&w=majority")
+db = client.earthquake
+collection = db['all_earthquake']
+
 import requests
 import tweepy
 import time
 import pandas as pd
 
 
-api_key = my_secret
-api_key_secret = my_secret
-access_token = my_secret
-access_token_secret = my_secret
+api_key = secret
+api_key_secret = secret
+access_token = secret
+access_token_secret = secret
 
 auth = tweepy.OAuthHandler(api_key, api_key_secret)
 auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth, wait_on_rate_limit = True)
+api = tweepy.API(auth, wait_on_rate_limit=True)
 
-big = opendata_url1
-big_en = opendata_url2
-small = opendata_url3
-small_en = opendataurl4
+big = secret
+big_en = secret
+small = secret
+small_en = secret
 
-def load(): #get data from central weather opendata api
+
+def load():
+    # get data from opendata api
     response_s = requests.get(small)
     res_ch = response_s.json()
+
     response_b = requests.get(big)
     res_b = response_b.json()
 
-#create a df that combine big and small earthquakes    
-    earthquake_time = [] 
+    earthquake_time = []  # 建立一個整合大小地震的df
     description = []
     uri = []
-    magtitude = []
-    #big earthquake
+    magnitude = []
+    # 大地震
     try:
         for i in range(100):
             time = res_b['records']['earthquake'][i]['earthquakeInfo']['originTime']
@@ -39,13 +47,13 @@ def load(): #get data from central weather opendata api
             description.append(content)
 
             mag = res_b['records']['earthquake'][i]['earthquakeInfo']['magnitude']['magnitudeValue']
-            magtitude.append(mag)
+            magnitude.append(mag)
 
             u = res_b['records']['earthquake'][i]['reportImageURI']
             uri.append(u)
     except IndexError:
         pass
-    #small earthquake
+    # 小區域地震
     try:
         for i in range(100):
             time = res_ch['records']['earthquake'][i]['earthquakeInfo']['originTime']
@@ -55,14 +63,14 @@ def load(): #get data from central weather opendata api
             description.append(content)
 
             mag = res_ch['records']['earthquake'][i]['earthquakeInfo']['magnitude']['magnitudeValue']
-            magtitude.append(mag)
+            magnitude.append(mag)
 
             u = res_ch['records']['earthquake'][i]['reportImageURI']
             uri.append(u)
     except IndexError:
         pass
 
-    data = {'time':earthquake_time, 'despriction':description, 'magtitude' : magtitude, 'uri':uri }
+    data = {'time': earthquake_time, 'despriction': description, 'magnitude': magnitude, 'uri': uri}
     df = pd.DataFrame(data)
     return df
 
@@ -70,27 +78,25 @@ def load(): #get data from central weather opendata api
 def post_new():
     userID = "EarthquakeTwtw"
     tweets = api.user_timeline(screen_name=userID,
-                           # 200 is the maximum allowed count
-                           count=10,
-                           include_rts = False,
-                           # Necessary to keep full_text
-                           # otherwise only the first 140 words are extracted
-                           tweet_mode = 'extended')
+                               # 200 is the maximum allowed count
+                               count=10,
+                               include_rts=False,
+                               # Necessary to keep full_text
+                               # otherwise only the first 140 words are extracted
+                               tweet_mode='extended')
     posted = []
     for info in tweets:
         latest_tweet = info.full_text[:11]
         posted.append(latest_tweet)
 
     print("我發過的")
-    print(posted, end = "\n ")
+    print(posted, end="\n ")
 
     df = load()
 
-    df['time'] = pd.to_datetime(df['time']) #change to datetime in order to sort
-    df_sort = df.sort_values('time', ascending = False).reset_index(drop = True)
-    
-
-    des = df_sort['despriction'][:6] #compare  6 rows of data from opendata
+    df['time'] = pd.to_datetime(df['time'])  # 轉換為了要排序
+    df_sort = df.sort_values('time', ascending=False).reset_index(drop=True)
+    des = df_sort['despriction'][:6]  # 比較最近六筆資料
 
     all_des = []
     for i in des:
@@ -98,23 +104,30 @@ def post_new():
         all_des.append(a)
 
     print("資料上最新")
-    print(all_des, end = "\n ")
+    print(all_des, end="\n ")
 
     a_set = set(posted)
     idx_b_minus_a = [idx for idx, val in enumerate(all_des) if val not in a_set]
-    idx_b_minus_a.reverse() 
-#find out recent earthquake's index and in the order of happening time
-    
-    print(idx_b_minus_a) #the index of earthquake that are new and without tweet before
+    idx_b_minus_a.reverse()  # 先找出發生的地震的索引值，才能依序發文
+
+    print(idx_b_minus_a)
 
     for i in idx_b_minus_a:
-        api.update_status(df_sort['despriction'][i] +df_sort['uri'][i], count)
-        #print(df_sort['despriction'][i] +df_sort['uri'][i], count)
+        time = df_sort['time'][i]
+        content = df_sort['despriction'][i]
+        mag = df_sort['magnitude'][i]
+        u = df_sort['uri'][i]
+
+        api.update_status(content + u, count)
         print('earthquake just happened', count)
+
+        post = {'time': time, 'despriction': content, 'magnitude': mag, 'uri': u}
+        collection.insert_one(post)
+
 
 count = 0
 while True:
     post_new()
     print(count)
     count += 1
-    time.sleep(600)
+    time.sleep(1800)
